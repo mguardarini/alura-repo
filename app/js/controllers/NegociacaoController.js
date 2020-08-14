20bf6015b2b5adf6a98245e5678ae8bc9a8e2a0a
Mauro Guardarini <mauro.guardarini@lambda3.com.br>
2020-08-13 23:07:13 -0300

    criado tabela

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,12 +1,18 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        // atualiza a view para exibir os dados do modelo, vazio
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
-        console.log(negociacao);
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociacoes.adiciona(negociacao);
+        // depois de adicionar, atualiza a view novamente para refletir os dados
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,25 +1,33 @@
- class NegociacaoController {
+class NegociacaoController {
 
-        private _inputData: HTMLInputElement;
-        private _inputQuantidade: HTMLInputElement;
-        private _inputValor: HTMLInputElement;
-        
-        constructor() {
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
-            this._inputData = <HTMLInputElement>document.querySelector('#data');
-            this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-            this._inputValor = <HTMLInputElement>document.querySelector('#valor');
-        }
+    constructor() {
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
 
-        adiciona(event: Event) {
-
-            event.preventDefault();
-
-            const negociacao = new Negociacao(
-                new Date(this._inputData.value.replace(/-/g, '/')),
-                parseInt(this._inputQuantidade.value),
-                parseFloat(this._inputValor.value));
-
-            console.log(negociacao);
-        }
+       // atualiza a view para exibir os dados do modelo, vazio
+        this._negociacoesView.update(this._negociacoes);
     }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        this._negociacoes.adiciona(negociacao);
+
+        // depois de adicionar, atualiza a view novamente para refletir os dados
+        this._negociacoesView.update(this._negociacoes);
+    }
+}
